ff801829c6001abafe9106c5d971855246196c79
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2025-01-15 13:45:08 +0600

    dynamic form and input created

---
+++ src/components/form/PHInputs.tsx
@@ -0,0 +1,19 @@
+import { Input } from 'antd';
+import { Controller } from 'react-hook-form';
+
+const PHInput = ({ type, name, label }) => {
+
+    return (
+        <div style={{ marginBottom: '16px' }}> 
+            {label && <label htmlFor={label}>{label}:</label>}
+            <Controller
+                name={name} render={({ field }) => (
+                    <Input {...field} type={type} id={name} />
+                )}
+            />
+
+        </div>
+    );
+};
+
+export default PHInput;
